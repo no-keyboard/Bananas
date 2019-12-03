bee209357ab37c06935426a80e9737ad18cf7ce8
no-keyboard <finaldestiny92@gmail.com>
2019-12-03 14:31:46 -0500

    Update PackageMakerAPISetter.js

--- PackageMakerAPISetter.js
+++ PackageMakerAPISetter.js
@@ -8,31 +8,30 @@
 // @grant        none
 // ==/UserScript==
 
+var loggedInLink = document.querySelector("#myUserInfo > a");
+var hoverText = String(loggedInLink.onmouseover);
+var instance = hoverText.substring(
+						hoverText.lastIndexOf("<br/>Instance:") + 15,
+						hoverText.lastIndexOf("<br/>Org Id:")
+						);
+loggedInLink.innerHTML = "FYI, YOU'RE LOGGED IN HERE: " + instance.bold();
+loggedInLink.style.color = "#ff0000";
+loggedInLink.style.fontSize = "large";
+
 if(window.location.href === "https://workbench.developerforce.com/login.php") {
 	document.querySelector("#oauth_env").value = 'test.salesforce.com';
 	document.querySelector("#termsAccepted").checked = true;
 	document.querySelector("#loginBtn").click();
 }
 
-if(window.location.href === "https://workbench.developerforce.com/select.php") {
-	var loggedInLink = document.querySelector("#myUserInfo > a");
-	var hoverText = String(loggedInLink.onmouseover);
-	var instance = hoverText.substring(
-							hoverText.lastIndexOf("<br/>Instance:") + 15,
-							hoverText.lastIndexOf("<br/>Org Id:")
-							);
-	loggedInLink.innerHTML = "FYI, YOU'RE LOGGED IN HERE: " + instance.bold();
-	loggedInLink.style.color = "#ff0000";
-}
-
-if(window.location.href === "https://workbench.developerforce.com/metadataDescribeAndList.php") {
+if(window.location.href.indexOf("https://workbench.developerforce.com/metadataDescribeAndList.php") > -1) {
 	const container = document.querySelector('body');
 	const mutationConfig = {
-		attributes: false,
-		childList: true,
-		subtree: true,
-		characterData: false,
-		characterDataOldValue: false
+					attributes: false,
+					childList: true,
+					subtree: true,
+					characterData: false,
+					characterDataOldValue: false
 	};
 
 	var onChange = function(mutationsList) {
@@ -41,7 +40,7 @@ if(window.location.href === "https://workbench.developerforce.com/metadataDescri
 				let xml = mutationsList[i].target;
 				if(xml.innerHTML.includes("version&gt;34.0")) {
 					//console.log(xml.innerHTML);
-					xml.innerHTML = xml.innerHTML.replace("version&gt;34.0", "version&gt;46.0");
+					xml.innerHTML = xml.innerHTML.replace("version&gt;34.0", "version&gt;47.0");
 				}
 			}
 		}
