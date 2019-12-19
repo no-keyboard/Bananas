ed41effa9445d8a4e29cf96bb35b7fd55b3e8750
no-keyboard <finaldestiny92@gmail.com>
2019-12-19 18:59:44 -0500

    Update JiraTagCopier.js

--- JiraTagCopier.js
+++ JiraTagCopier.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://abcwebrepo.amerisourcebergen.com/projects/DAT/repos/client-gskpap/commits/*
+
 // @grant        none
 // ==/UserScript==
 
