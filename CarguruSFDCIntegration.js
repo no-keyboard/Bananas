1f72b86bdbd4a3be570895d08b1a3e5a82e66046
no-keyboard <finaldestiny92@gmail.com>
2019-12-25 00:01:26 -0500

    Update CarguruSFDCIntegration.js

--- CarguruSFDCIntegration.js
+++ CarguruSFDCIntegration.js
@@ -71,21 +71,82 @@ if(window.location.href === "https://www.cargurus.com/") {
 	zip.value = zipValue;
 }
 
-if(window.location.href.indexOf("https://www.cargurus.com/Cars/inventorylisting/") > -1 && window.location.href.indexOf("#listing=") === -1) {
+// if(window.location.href.indexOf("https://www.cargurus.com/Cars/inventorylisting/") > -1 && window.location.href.indexOf("#listing=") === -1) {
+// 	const listingSearch = document.querySelector("#cargurus-listing-search");
+
+// 	const callback = function(mutationsList, observer) {
+// 		for(let mutation of mutationsList) {
+// 			console.log(mutation);
+// 		}
+// 	}
+
+// 	const observer = new MutationObserver(callback);
+// 	observer.observe(listingSearch, config);
+// 	// const gt4Checkbox = document.querySelector("#TRIM_NAME-GT4");
+// 	// gt4Checkbox.checked = true;
+// }
+
+if(window.location.href.indexOf("https://www.cargurus.com/Cars/inventorylisting/") > -1 && window.location.href.indexOf("#listing=") > -1) {
 	const listingSearch = document.querySelector("#cargurus-listing-search");
+	let sidebarButtons;
+	let integrationBtn
+
+	const sfdcPost = function(listingObj) {
+		console.log("json to post", listingObj);
+	}
+
+	const addButton = function() {
+		integrationBtn = sidebarButtons.querySelector("div").cloneNode(true);
+		integrationBtn.querySelector("button > span").innerHTML = "Salesforce";
+		integrationBtn.querySelector("button > span > svg").remove();
+		let icon = document.createElement("span");
+		icon.innerHTML = "&#9729;";
+		integrationBtn.querySelector("button > span").appendChild(icon);
+		//console.log(integrationBtn.querySelector("button > span > svg"));
+		sidebarButtons.appendChild(integrationBtn);
+	}
+
+	const getListingSummaryVal = function(table, value) {
+		let tableLabels = table.querySelectorAll("dt");
+		let tableValues = table.querySelectorAll("dd");
+		for(let i = 0; i < tableLabels.length; i++) {
+			if(tableLabels[i].innerText.slice(0, -1) === value) {
+				return tableValues[i];
+			}
+		}
+	}
+
+	const parseListing = function() {
+		let listingSummary = listingSearch.querySelector("div._43PKvB > div._36TanG > div._24ffzL > div._5jSLnT > section > h2").parentNode.querySelector("dl");
+		let listingInfo = new Object();
+		listingInfo.name = listingSearch.querySelector("div._43PKvB > div._36TanG > div._24ffzL > div._3Wnbei > div:nth-child(3) > section > div:nth-child(1) > form > p > strong").innerText;
+		listingInfo.price = getListingSummaryVal(listingSummary, "Dealer's Price").querySelector("strong").innerText.replace("$", "").replace(",", "");
+		listingInfo.miles = getListingSummaryVal(listingSummary, "Mileage").innerText.replace(",", "").replace(" miles", "");
+		listingInfo.color = getListingSummaryVal(listingSummary, "Exterior Color").innerText;
+		listingInfo.vin = getListingSummaryVal(listingSummary, "VIN").innerText;
+		listingInfo.trans = getListingSummaryVal(listingSummary, "Transmission").innerText;
+		listingInfo.url = window.location.href;
+		//todo get the listing's age
+		listingInfo.postAge = null
+		console.log("parsed listing", listingInfo);
+		return listingInfo;
+	}
 
 	const callback = function(mutationsList, observer) {
 		for(let mutation of mutationsList) {
-			console.log(mutation);
+			if(listingSearch.querySelector("div > div > div > div > div > button > span") !== null) {
+				sidebarButtons = listingSearch.querySelector("div > div > div > div > div > button > span").parentNode.parentNode.parentNode;
+				//console.log(sidebarButtons);
+				observer.disconnect();
+				break;
+			}
 		}
+
+		addButton();
+		let listingDetails = parseListing();
+		sfdcPost(listingDetails);
 	}
 
 	const observer = new MutationObserver(callback);
 	observer.observe(listingSearch, config);
-	// const gt4Checkbox = document.querySelector("#TRIM_NAME-GT4");
-	// gt4Checkbox.checked = true;
-}
-
-if(window.location.href.indexOf("https://www.cargurus.com/Cars/inventorylisting/") > -1 && window.location.href.indexOf("#listing=") > -1) {
-
 }
