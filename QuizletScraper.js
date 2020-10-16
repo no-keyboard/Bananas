301864d09863ad99d43b2628e3ac94005ccba3f8
no-keyboard <finaldestiny92@gmail.com>
2020-10-15 20:53:05 -0400

    Update QuizletScraper.js

--- QuizletScraper.js
+++ QuizletScraper.js
@@ -8,41 +8,43 @@
 // @grant        none
 // ==/UserScript==
 
-const cardList = document.querySelector("#SetPageTarget > div > div.SetPage-setDetails > div.SetPage-setDetailsTermsWrapper > div > div > section > div > section");
+(() => {
+	const cardList = document.querySelector("#SetPageTarget > div > div.SetPage-setDetails > div.SetPage-setDetailsTermsWrapper > div > div > section > div > section");
 
-const exportCards = () => {
-	//create new array to store questions
-	let csv = [];
+	const exportCards = () => {
+		//create new array to store questions
+		let csv = [];
 
-	cardList.childNodes.forEach((card, i) => {
-		let divChildSelect = i + 1;
-		let question = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > span > span");
+		cardList.childNodes.forEach((card, i) => {
+			let divChildSelect = i + 1;
+			let question = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > span > span");
 
-		//for some reason some questions are clickable
-		if(question == null) {
-			question = document.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > a > span").innerHTML;
-		} else {
-			question = question.innerHTML;
-		}
-				   
-		let answer = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-smallSide > div > a > span").innerHTML;
-		
-		//create temp object to collect values
-		let questionSet = {};
-		questionSet.question = question;
-		// add commas between characters
-		questionSet.answer = answer.split("").join(",");
-		csv.push(questionSet);
+			//for some reason some questions are clickable
+			if(question == null) {
+				question = document.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > a > span").innerHTML;
+			} else {
+				question = question.innerHTML;
+			}
+					   
+			let answer = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-smallSide > div > a > span").innerHTML;
+			
+			//create temp object to collect values
+			let questionSet = {};
+			questionSet.question = question;
+			// add commas between characters
+			questionSet.answer = answer.split("").join(",");
+			csv.push(questionSet);
 
-		// console.log("question #: ", divChildSelect);
-		// console.log(question);
-		// console.log("answer: ", answer);
-	});
+			// console.log("question #: ", divChildSelect);
+			// console.log(question);
+			// console.log("answer: ", answer);
+		});
 
-	console.log("object", JSON.stringify(csv));
-}
+		console.log("object", JSON.stringify(csv));
+	}
 
-const exportBtn = document.createElement("button");
-exportBtn.innerHTML = "Export Cards";
-exportBtn.onclick = exportCards;
-document.body.prepend(exportBtn);
+	const exportBtn = document.createElement("button");
+	exportBtn.innerHTML = "Export Cards";
+	exportBtn.onclick = exportCards;
+	document.body.prepend(exportBtn);
+});
