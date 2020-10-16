c6dae3005ff41aad2b59fa654912709f443d4c9d
no-keyboard <finaldestiny92@gmail.com>
2020-10-15 23:31:06 -0400

    Update QuizletScraper.js

--- QuizletScraper.js
+++ QuizletScraper.js
@@ -9,38 +9,43 @@
 // ==/UserScript==
 
 (() => {
-	const cardList = document.querySelector("#SetPageTarget > div > div.SetPage-setDetails > div.SetPage-setDetailsTermsWrapper > div > div > section > div > section");
-
 	const exportCards = () => {
+        const cardList = document.querySelector("#setPageSetDetails > div.SetPage-setDetailsTermsWrapper > div > div:nth-child(2) > div > div > section > div > section");
+
 		//create new array to store questions
 		let csv = [];
 
-		cardList.childNodes.forEach((card, i) => {
-			let divChildSelect = i + 1;
-			let question = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > span > span");
+		cardList.childNodes.forEach((card) => {
+			try {
+				//console.log(card);
+				let question = card.querySelector("div.SetPageTerm-smallSide");
+				let answer = card.querySelector("div.SetPageTerm-largeSide");
 
-			//for some reason some questions are clickable
-			if(question == null) {
-				question = document.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-largeSide > div > a > span").innerHTML;
-			} else {
-				question = question.innerHTML;
+				//for some reason some questions are clickable
+				if(question) {
+					question = question.querySelector("span.TermText").innerHTML;
+				} else {
+					question = card.querySelectorAll("div.SetPageTerm-sideContent")[0].querySelector("span.TermText").innerHTML;
+				}
+
+				if(answer) {
+					answer = answer.querySelector("span.TermText").innerHTML;
+				} else {
+					answer = card.querySelectorAll("div.SetPageTerm-sideContent")[1].querySelector("span.TermText").innerHTML;
+				}
+
+				//create temp object to collect values
+				let questionSet = {};
+				questionSet.question = question;
+				// add commas between characters
+				questionSet.answer = answer;
+				csv.push(questionSet);
+			} catch(err) {
+				console.log(err, card);
 			}
-					   
-			let answer = card.querySelector("div:nth-child(" + divChildSelect + ") > div > div > div.SetPageTerm-contentWrapper > div > div.SetPageTerm-side.SetPageTerm-smallSide > div > a > span").innerHTML;
-			
-			//create temp object to collect values
-			let questionSet = {};
-			questionSet.question = question;
-			// add commas between characters
-			questionSet.answer = answer.split("").join(",");
-			csv.push(questionSet);
-
-			// console.log("question #: ", divChildSelect);
-			// console.log(question);
-			// console.log("answer: ", answer);
 		});
 
-		console.log("object", JSON.stringify(csv));
+		console.log(JSON.stringify(csv));
 	}
 
 	const exportBtn = document.createElement("button");
